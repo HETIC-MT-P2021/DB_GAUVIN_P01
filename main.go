56a4978610a37fa27357670ae524623679c29310
jasongauvin <contact.jason.gauvin@gmail.com>
2020-10-10 14:43:37 +0200

    refacto main

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"log"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	cors "github.com/itsjamie/gin-cors"
 	"github.com/jasongauvin/DB_GAUVIN_P01/database"
 
 	"github.com/jasongauvin/DB_GAUVIN_P01/router"
 )
 
 func main() {
-	time.Sleep(10 * time.Second)
+
+	time.Sleep(5 * time.Second)
+
 	database.InitializeDb()
-	router.InitRouter()
+
+	r := gin.Default()
+
+	r.Use(cors.Middleware(cors.Config{
+		Origins:         "*",
+		Methods:         "GET, PUT, POST, DELETE",
+		RequestHeaders:  "Origin, Authorization, Content-Type",
+		ExposedHeaders:  "Authorization",
+		MaxAge:          50 * time.Second,
+		Credentials:     true,
+		ValidateHeaders: false,
+	}))
+
+	router.InitRouter(r)
+
+	log.Fatal(r.Run(":8080")) // listen and serve on 8080
 }
