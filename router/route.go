0aa2d99c416e8c6724af75e6525da05412b6de89
jasongauvin <contact.jason.gauvin@gmail.com>
2020-10-11 12:45:31 +0200

    Add route for GetEmployeeByOfficeCode

--- router/route.go
+++ router/route.go
@@ -14,13 +14,8 @@ func InitRouter(router *gin.Engine) {
 
 		v1 := api.Group("/v1")
 		{
-			v1.GET("/employee", controllers.ShowEmployees)
-			// v1.GET("/customers/:number", controllers.GetCustomerByNumber)
-			// v1.GET("/customers/:number/orders", controllers.GetOrdersByCustomerNumber)
-			// v1.GET("/orders/:number", controllers.GetOrderByNumber)
-			// v1.GET("/orders/:number/details", controllers.GetOrderDetailsByOrderNumber)
-			// v1.GET("/offices/:code/employees", controllers.GetEmployeesByOfficeCode)
-			// v1.GET("/offices/:code", controllers.GetOfficeByCode)
+			v1.GET("/employee", controllers.GetEmployees)
+			v1.GET("/offices/:id/employees", controllers.GetEmployeesByOfficeCode)
 		}
 	}
 }
