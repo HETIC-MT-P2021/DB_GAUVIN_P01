08b237740656056fa212e1e0daf20e4fd075322c
jasongauvin <contact.jason.gauvin@gmail.com>
2020-10-12 22:04:42 +0200

    Add customer structure

--- models/customers_struc.go
+++ models/customers_struc.go
@@ -1,17 +1,19 @@
 package models
 
-type Customer struc {
-	Number                 uint64 	`json:"customerNumber"`
-	Name                   string 	`json:"customerName"`
-	FirstName              string 	`json:"customerFirstName"`
-	LastName               string 	`json:"customerLastName"`
-	Phone                  string 	`json:"phone"`
-	AddressLine1           string 	`json:"addressLine1"`
-	AddressLine2           string 	`json:"addressLine2"`
-	City                   string 	`json:"city"`
-	State                  string 	`json:"state"`
-	PostCode               string 	`json:"postalCode"`
-	Country                string 	`json:"country"`
-	SalesRepEmployeeNumber uint64 	`json:"salesRepEmployeeNumber"`
-	CreditLimit            float64 	`json:"creditLimit"`
+type Customer struct {
+	CustomerNumber         uint64  `json:"customerNumber"`
+	CustomerName           string  `json:"customerName"`
+	ContactFirstName       string  `json:"contactFirstName"`
+	ContactLastName        string  `json:"contactLastName"`
+	Phone                  string  `json:"phone"`
+	AddressLine1           string  `json:"addressLine1"`
+	AddressLine2           string  `json:"addressLine2"`
+	City                   string  `json:"city"`
+	State                  string  `json:"state"`
+	PostCode               string  `json:"postalCode"`
+	Country                string  `json:"country"`
+	SalesRepEmployeeNumber uint64  `json:"salesRepEmployeeNumber"`
+	CreditLimit            float64 `json:"creditLimit"`
+	OrderNumber            uint64  `json:"orderNumber"`
+	QuantityOrdered        uint64  `json:"quantityOrdered"`
 }
