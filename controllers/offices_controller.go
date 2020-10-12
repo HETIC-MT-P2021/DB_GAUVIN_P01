5476d984c08fe0eca58b9d3b9a6985cecfd6c7f7
jasongauvin <contact.jason.gauvin@gmail.com>
2020-10-12 21:12:30 +0200

    Add offices controller

---
+++ controllers/offices_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/jasongauvin/DB_GAUVIN_P01/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jasongauvin/DB_GAUVIN_P01/repository"
+	"github.com/jasongauvin/DB_GAUVIN_P01/utils"
+)
+
+func GetOfficeByCode(c *gin.Context) {
+	var offices *models.Office
+	var err error
+	id := utils.ParseStringToUint64(c.Param("id"))
+
+	offices, err = repository.FindOfficeByCode(id)
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, "Could'nt fetch offices by code.")
+		return
+	}
+
+	c.JSON(http.StatusOK, offices)
+}
