b7c56516b8b262f58574e9a131fece3d44fcf683
jasongauvin <contact.jason.gauvin@gmail.com>
2020-10-12 21:11:54 +0200

    Debug connector db

--- database/sql_connector.go
+++ database/sql_connector.go
@@ -11,6 +11,9 @@ var DB *sql.DB
 
 // InitializeDb connects to database
 func InitializeDb() {
+
+	var err error
+
 	dbDriver := "mysql"
 	dbUser := "gobdd"
 	dbPass := "gobdd"
@@ -18,12 +21,14 @@ func InitializeDb() {
 	dbHost := "db" // container = ip du container
 	dbPort := 3306
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
-	DB, err := sql.Open(dbDriver, dsn)
-	fmt.Println(DB)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
+	DB, err = sql.Open(dbDriver, dsn)
+
 	// if there is an error opening the connection, handle it
 	if err != nil {
 		panic(err.Error())
+	} else {
+		fmt.Println("Connected to database")
 	}
 
 }
