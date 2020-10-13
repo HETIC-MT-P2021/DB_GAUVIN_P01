d5f7824fb4fd6a3021f92b6d0b80f2d3653ba796
jasongauvin <contact.jason.gauvin@gmail.com>
2020-10-13 12:35:56 +0200

    Refacto employee

--- models/employees_struc.go
+++ models/employees_struc.go
@@ -1,12 +1,16 @@
 package models
 
+import (
+	"github.com/jasongauvin/DB_GAUVIN_P01/utils"
+)
+
 type Employee struct {
-	EmployeeNumber uint64 `json:"employeeNumber"`
-	LastName       string `json:"lastName"`
-	FirstName      string `json:"firstName"`
-	Extension      string `json:"extention"`
-	Email          string `json:"email"`
-	OfficeCode     string `json:"officeCode"`
-	JobTitle       string `json:"jobTitle"`
-	ReportsTo      string `json:"reportsCode"`
+	EmployeeNumber uint64           `json:"employeeNumber"`
+	LastName       string           `json:"lastName"`
+	FirstName      string           `json:"firstName"`
+	Extension      string           `json:"extention"`
+	Email          string           `json:"email"`
+	OfficeCode     string           `json:"officeCode"`
+	JobTitle       string           `json:"jobTitle"`
+	ReportsTo      utils.NullString `json:"reportsCode"`
 }
--- repository/employees_repository.go
+++ repository/employees_repository.go
@@ -2,17 +2,12 @@ package repository
 
 import (
 	"github.com/jasongauvin/DB_GAUVIN_P01/database"
-	"github.com/jasongauvin/DB_GAUVIN_P01/utils"
 
 	"github.com/jasongauvin/DB_GAUVIN_P01/models"
 )
 
 func FindEmployees() (*[]models.Employee, error) {
-	var (
-		EmployeeNumber                                              uint64
-		LastName, FirstName, Extension, Email, OfficeCode, JobTitle string
-		ReportsTo                                                   utils.NullString
-	)
+	employee := models.Employee{}
 
 	rows, err := database.DB.Query("SELECT * FROM employees")
 
@@ -25,26 +20,16 @@ func FindEmployees() (*[]models.Employee, error) {
 
 	for rows.Next() {
 		err = rows.Scan(
-			&EmployeeNumber,
-			&LastName,
-			&FirstName,
-			&Extension,
-			&Email,
-			&OfficeCode,
-			&JobTitle,
-			&ReportsTo,
+			&employee.EmployeeNumber,
+			&employee.LastName,
+			&employee.FirstName,
+			&employee.Extension,
+			&employee.Email,
+			&employee.OfficeCode,
+			&employee.JobTitle,
+			&employee.ReportsTo,
 		)
 
-		employee := models.Employee{
-			EmployeeNumber: EmployeeNumber,
-			LastName:       LastName,
-			FirstName:      FirstName,
-			Extension:      Extension,
-			Email:          Email,
-			OfficeCode:     OfficeCode,
-			JobTitle:       JobTitle,
-			ReportsTo:      ReportsTo.String,
-		}
 		employees = append(employees, employee)
 	}
 	if err != nil {
@@ -55,11 +40,7 @@ func FindEmployees() (*[]models.Employee, error) {
 }
 
 func FindEmployeesByOffice(code uint64) (*[]models.Employee, error) {
-	var (
-		EmployeeNumber                                              uint64
-		LastName, FirstName, Extension, Email, OfficeCode, JobTitle string
-		ReportsTo                                                   utils.NullString
-	)
+	employee := models.Employee{}
 
 	rows, err := database.DB.Query("SELECT e.employeeNumber, e.firstName, e.lastName, e.extension, e.email, e.officeCode, e.jobTitle, e.reportsTo FROM employees e INNER JOIN offices o ON e.officeCode = o.officeCode WHERE o.officeCode = ?;", code)
 
@@ -73,26 +54,15 @@ func FindEmployeesByOffice(code uint64) (*[]models.Employee, error) {
 
 	for rows.Next() {
 		err = rows.Scan(
-			&EmployeeNumber,
-			&LastName,
-			&FirstName,
-			&Extension,
-			&Email,
-			&OfficeCode,
-			&JobTitle,
-			&ReportsTo,
+			&employee.EmployeeNumber,
+			&employee.LastName,
+			&employee.FirstName,
+			&employee.Extension,
+			&employee.Email,
+			&employee.OfficeCode,
+			&employee.JobTitle,
+			&employee.ReportsTo,
 		)
-
-		employee := models.Employee{
-			EmployeeNumber: EmployeeNumber,
-			LastName:       LastName,
-			FirstName:      FirstName,
-			Extension:      Extension,
-			Email:          Email,
-			OfficeCode:     OfficeCode,
-			JobTitle:       JobTitle,
-			ReportsTo:      ReportsTo.String,
-		}
 
 		employees = append(employees, employee)
 	}
